5cf5d9e7345e8245fd0b84d85422323394647bc7
Toby Sullivan <tobyjsullivan@gmail.com>
2015-08-04 23:16:54 -0700

    Write first test for POST /people endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
+var request = require('request');
+var Mocha = require('mocha');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { results: ['Not run'] });
+});
+
+function runMochaTests(callback) {
+  var mocha = new Mocha();
+
+  mocha.addFile('test/people-tests.js');
+
+  var output = [];
+
+  mocha.run(function () {
+
+    if (callback != undefined) {
+      callback(output);
+    }
+  }).on('pass', function(test) {
+    output.push({status: "PASSED"});
+    console.log(test);
+  }).on('fail', function(test) {
+    output.push({status:"FAILED",detail:test.err.message});
+
+    console.log(test);
+  });
+}
+
+/* GET home page. */
+router.post('/runs', function(req, res, next) {
+  runMochaTests(function (output) {
+    res.render('index', { results: output });
+  });
 });
 
 module.exports = router;
---
+++ test/people-tests.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect;
+var request = require('request');
+
+describe("/api", function() {
+  var apiUrl = 'https://TK.herokuapp.com/';
+  describe("/people", function () {
+    describe("POST", function() {
+      it("should create a new person object", function(done) {
+        request.post(apiUrl+'people', function (error, response, body) {
+          expect(error).to.not.exist;
+          expect(response).to.exist;
+          expect(response.statusCode).to.equal(200);
+          expect(body).to.exist;
+          var personObj = JSON.parse(body);
+          expect(personObj).to.exist;
+          expect(personObj).to.have.a.property("personId");
+          expect(personObj.personId).to.match(/[a-zA-Z0-9\-]+/);
+          expect(personObj).to.have.a.property("name");
+          expect(personObj.name).to.match(/[a-zA-Z0-9]+/);
+          done();
+        });
+      });
+    });
+  });
+});
